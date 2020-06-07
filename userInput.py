2a205e87e79b4106b87c4ca0ed241a72b4906fe2
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2020-06-07 20:21:48 +0530

    Add: Created heroku app

--- userInput.py
+++ userInput.py
@@ -4,7 +4,5 @@ parser =argparse.ArgumentParser()
 # parser.add_argument("-w", "-weight")
 group1 = parser.add_mutually_exclusive_group()
 group1.add_argument("-tr", "--train", help="Training", action='store_true')
-
-group2 = parser.add_mutually_exclusive_group()
-group2.add_argument("-te", "--test", help="Testing", action='store_true')
+group1.add_argument("-te", "--test", help="Testing", action='store_true')
 args = parser.parse_args()
